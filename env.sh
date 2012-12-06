69e787d0abb27d81cadec1bdded30afb1f2c878d
Kyle Isom <coder@kyleisom.net>
2012-12-05 18:27:36 -0700

    removing old code for rewrite

--- env.sh
+++
@@ -1,9 +0,0 @@
-# test environment set up
-
-export OPMLFEED_REDIS_DB=0
-export OPMLFEED_SSLKEY="$(pwd)/ssl-keys/test.key"
-export OPMLFEED_SSLCERT="$(pwd)/ssl-keys/test.cert"
-export REDIS_ADDR=""
-export REDIS_PASS=""
-export SERVER_ADDRESS=""
-export SERVER_PORT="8443"
